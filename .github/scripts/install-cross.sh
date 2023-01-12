d0b0bcb16c89325d534b902a6c902a9c8c2f9212
Joonas Javanainen <joonas.javanainen@gmail.com>
2023-01-12 23:25:13 +0200

    CI

--- .github/scripts/install-cross.sh
+++ .github/scripts/install-cross.sh
@@ -9,6 +9,6 @@ set -euo pipefail
 RELEASE_FILE="cross-x86_64-unknown-linux-gnu.tar.gz"
 
 mkdir -p ${HOME}/bin
-curl -sSL "https://github.com/cross-rs/cross/releases/download/${CROSS_VERSION}/${RELEASE_FILE}"
+curl -sSLO "https://github.com/cross-rs/cross/releases/download/v${CROSS_VERSION}/${RELEASE_FILE}"
 echo "${CROSS_SHA256}  ${RELEASE_FILE}" | sha256sum
-tar xzv -C "${HOME}/bin" "${RELEASE_FILE}"
+cat "${RELEASE_FILE}" | tar xzv -C "${HOME}/bin"
